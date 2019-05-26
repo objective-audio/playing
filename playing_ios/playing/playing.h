cc11117900085c322f4e00b2500aac59ba1453ac
Yuki Yasoshima <yukiyasos@gmail.com>
2019-05-26 20:32:52 +0900

    add playing_ios project

---
+++ playing_ios/playing/playing.h
@@ -0,0 +1,13 @@
+//
+//  playing.h
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for playing.
+FOUNDATION_EXPORT double playingVersionNumber;
+
+//! Project version string for playing.
+FOUNDATION_EXPORT const unsigned char playingVersionString[];
+
+#import <playing/yas_playing_umbrella.h>
