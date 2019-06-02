a89b57d8df52608209f9add2b16018edfa615e7d
Yuki Yasoshima <yukiyasos@gmail.com>
2019-06-02 15:24:38 +0900

    add sample_controller

---
+++ playing_ios_sample/playing_ios_sample/yas_playing_sample_controller.cpp
@@ -0,0 +1,5 @@
+//
+//  yas_playing_sample_controller.cpp
+//
+
+#include "yas_playing_sample_controller.hpp"
---
+++ playing_ios_sample/playing_ios_sample/yas_playing_sample_controller.hpp
@@ -0,0 +1,20 @@
+//
+//  yas_playing_sample_controller.hpp
+//
+
+#pragma once
+
+#include <audio/yas_audio_umbrella.h>
+#include <cpp_utils/yas_cpp_utils_umbrella.h>
+#include <playing/yas_playing_umbrella.h>
+#include <processing/yas_processing_umbrella.h>
+
+namespace yas::playing::sample {
+    struct controller {
+        std::string root_path =
+        file_path{system_path_utils::directory_path(system_path_utils::dir::document)}.appending("sample").string();
+        audio_coordinator coordinator{this->root_path};
+        playing::timeline_exporter timeline_exporter{this->root_path, task_queue{2}, {.timeline = 0, .fragment = 1}, this->coordinator.sample_rate()};
+        chaining::observer_pool pool;
+    };
+}
