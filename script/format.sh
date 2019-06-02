97db1aed2f625f64a4920e88b4c0435fb76dd4d9
Yuki Yasoshima <yukiyasos@gmail.com>
2019-06-02 15:51:44 +0900

    update format.sh

--- script/format.sh
+++ script/format.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-clang-format -i -style=file `find ../playing ../playing_tests -type f \( -name *.h -o -name *.cpp -o -name *.hpp -o -name *.m -o -name *.mm \)`
+clang-format -i -style=file `find ../playing ../playing_tests ../playing_ios_sample/playing_ios_sample -type f \( -name *.h -o -name *.cpp -o -name *.hpp -o -name *.m -o -name *.mm \)`
