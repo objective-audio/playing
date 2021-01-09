d4e10a35a9f8e28e0013f1ab16e1f97b31344a20
Yuki Yasoshima <yukiyasos@gmail.com>
2021-01-09 21:29:06 +0900

    add test file

---
+++ playing_tests/yas_playing_coordinator_tests.mm
@@ -0,0 +1,13 @@
+//
+//  yas_playing_coordinator_tests.mm
+//
+
+#import <XCTest/XCTest.h>
+
+@interface yas_playing_coordinator_tests : XCTestCase
+
+@end
+
+@implementation yas_playing_coordinator_tests
+
+@end
