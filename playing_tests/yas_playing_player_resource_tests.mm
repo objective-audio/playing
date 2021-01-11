0a2fbaefebc9d6b3ed69ca6a0bb0ec751b12ad54
Yuki Yasoshima <yukiyasos@gmail.com>
2021-01-11 17:05:09 +0900

    add player_resource_tests

---
+++ playing_tests/yas_playing_player_resource_tests.mm
@@ -0,0 +1,14 @@
+//
+//  yas_playing_player_resource_tests.mm
+//
+
+#import <XCTest/XCTest.h>
+#import <playing/playing.h>
+
+@interface yas_playing_player_resource_tests : XCTestCase
+
+@end
+
+@implementation yas_playing_player_resource_tests
+
+@end
