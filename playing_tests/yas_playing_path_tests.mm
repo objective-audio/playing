428607e509857b95c230b3bf8bd3a01a5fe38949
Yuki Yasoshima <yukiyasos@gmail.com>
2021-01-13 22:32:44 +0900

    path test

--- playing_tests/yas_playing_path_tests.mm
+++ playing_tests/yas_playing_path_tests.mm
@@ -1,5 +1,5 @@
 //
-//  yas_playing_url_tests.mm
+//  yas_playing_path_tests.mm
 //
 
 #import <XCTest/XCTest.h>
@@ -23,6 +23,18 @@ using namespace yas::playing;
     XCTAssertEqual(tl_path.string(), "/root/0_48000");
 }
 
+- (void)test_timeline_equal {
+    XCTAssertTrue((path::timeline{"/root", "0", 48000}) == (path::timeline{"/root", "0", 48000}));
+    XCTAssertFalse((path::timeline{"/root", "0", 48000}) == (path::timeline{"/other", "0", 48000}));
+    XCTAssertFalse((path::timeline{"/root", "0", 48000}) == (path::timeline{"/root", "1", 48000}));
+    XCTAssertFalse((path::timeline{"/root", "0", 48000}) == (path::timeline{"/root", "0", 48001}));
+
+    XCTAssertFalse((path::timeline{"/root", "0", 48000}) != (path::timeline{"/root", "0", 48000}));
+    XCTAssertTrue((path::timeline{"/root", "0", 48000}) != (path::timeline{"/other", "0", 48000}));
+    XCTAssertTrue((path::timeline{"/root", "0", 48000}) != (path::timeline{"/root", "1", 48000}));
+    XCTAssertTrue((path::timeline{"/root", "0", 48000}) != (path::timeline{"/root", "0", 48001}));
+}
+
 - (void)test_channel {
     path::timeline tl_path{"/root", "0", 48000};
     path::channel ch_path{tl_path, 1};
@@ -31,12 +43,44 @@ using namespace yas::playing;
     XCTAssertEqual(ch_path.string(), "/root/0_48000/1");
 }
 
+- (void)test_channel_equal {
+    XCTAssertTrue((path::channel{path::timeline{"/root", "0", 48000}, 1}) ==
+                  (path::channel{path::timeline{"/root", "0", 48000}, 1}));
+    XCTAssertFalse((path::channel{path::timeline{"/root", "1", 48000}, 1}) ==
+                   (path::channel{path::timeline{"/root", "0", 48000}, 1}));
+    XCTAssertFalse((path::channel{path::timeline{"/root", "0", 48000}, 1}) ==
+                   (path::channel{path::timeline{"/root", "0", 48000}, 2}));
+
+    XCTAssertFalse((path::channel{path::timeline{"/root", "0", 48000}, 1}) !=
+                   (path::channel{path::timeline{"/root", "0", 48000}, 1}));
+    XCTAssertTrue((path::channel{path::timeline{"/root", "0", 48000}, 1}) !=
+                  (path::channel{path::timeline{"/root", "1", 48000}, 1}));
+    XCTAssertTrue((path::channel{path::timeline{"/root", "0", 48000}, 1}) !=
+                  (path::channel{path::timeline{"/root", "0", 48000}, 2}));
+}
+
 - (void)test_fragment {
     path::timeline tl_path{"/root", "0", 48000};
     path::fragment frag_path{path::channel{tl_path, 1}, 2};
 
     XCTAssertEqual(frag_path.fragment_index, 2);
     XCTAssertEqual(frag_path.string(), "/root/0_48000/1/2");
+}
+
+- (void)test_fragment_equal {
+    XCTAssertTrue((path::fragment{path::channel{path::timeline{"/root", "0", 48000}, 1}, 2}) ==
+                  (path::fragment{path::channel{path::timeline{"/root", "0", 48000}, 1}, 2}));
+    XCTAssertFalse((path::fragment{path::channel{path::timeline{"/root", "0", 48000}, 1}, 2}) ==
+                   (path::fragment{path::channel{path::timeline{"/root", "0", 48000}, 3}, 2}));
+    XCTAssertFalse((path::fragment{path::channel{path::timeline{"/root", "0", 48000}, 1}, 2}) ==
+                   (path::fragment{path::channel{path::timeline{"/root", "0", 48000}, 1}, 4}));
+
+    XCTAssertFalse((path::fragment{path::channel{path::timeline{"/root", "0", 48000}, 1}, 2}) !=
+                   (path::fragment{path::channel{path::timeline{"/root", "0", 48000}, 1}, 2}));
+    XCTAssertTrue((path::fragment{path::channel{path::timeline{"/root", "0", 48000}, 1}, 2}) !=
+                  (path::fragment{path::channel{path::timeline{"/root", "0", 48000}, 3}, 2}));
+    XCTAssertTrue((path::fragment{path::channel{path::timeline{"/root", "0", 48000}, 1}, 2}) !=
+                  (path::fragment{path::channel{path::timeline{"/root", "0", 48000}, 1}, 4}));
 }
 
 - (void)test_signal_event {
@@ -50,6 +94,30 @@ using namespace yas::playing;
     XCTAssertEqual(signal_event_path.string(), "/root/0_48000/1/2/signal_3_4_i64");
 }
 
+- (void)test_signal_event_equal {
+    path::fragment const frag_path_1a{path::channel{path::timeline{"/root", "0", 48000}, 1}, 2};
+    path::fragment const frag_path_1b{path::channel{path::timeline{"/root", "0", 48000}, 1}, 2};
+    path::fragment const frag_path_2{path::channel{path::timeline{"/root", "0", 48000}, 1}, 3};
+
+    XCTAssertTrue((path::signal_event{frag_path_1a, {3, 4}, typeid(int64_t)}) ==
+                  (path::signal_event{frag_path_1b, {3, 4}, typeid(int64_t)}));
+    XCTAssertFalse((path::signal_event{frag_path_1a, {3, 4}, typeid(int64_t)}) ==
+                   (path::signal_event{frag_path_2, {3, 4}, typeid(int64_t)}));
+    XCTAssertFalse((path::signal_event{frag_path_1a, {3, 4}, typeid(int64_t)}) ==
+                   (path::signal_event{frag_path_1b, {3, 5}, typeid(int64_t)}));
+    XCTAssertFalse((path::signal_event{frag_path_1a, {3, 4}, typeid(int64_t)}) ==
+                   (path::signal_event{frag_path_1b, {3, 4}, typeid(float)}));
+
+    XCTAssertFalse((path::signal_event{frag_path_1a, {3, 4}, typeid(int64_t)}) !=
+                   (path::signal_event{frag_path_1b, {3, 4}, typeid(int64_t)}));
+    XCTAssertTrue((path::signal_event{frag_path_1a, {3, 4}, typeid(int64_t)}) !=
+                  (path::signal_event{frag_path_2, {3, 4}, typeid(int64_t)}));
+    XCTAssertTrue((path::signal_event{frag_path_1a, {3, 4}, typeid(int64_t)}) !=
+                  (path::signal_event{frag_path_1b, {3, 5}, typeid(int64_t)}));
+    XCTAssertTrue((path::signal_event{frag_path_1a, {3, 4}, typeid(int64_t)}) !=
+                  (path::signal_event{frag_path_1b, {3, 4}, typeid(float)}));
+}
+
 - (void)test_number_events {
     path::timeline tl_path{"/root", "0", 48000};
     path::channel ch_path{tl_path, 1};
@@ -57,6 +125,18 @@ using namespace yas::playing;
     path::number_events number_events_path{frag_path};
 
     XCTAssertEqual(number_events_path.string(), "/root/0_48000/1/2/numbers");
+}
+
+- (void)test_number_events_equal {
+    path::fragment const frag_path_1a{path::channel{path::timeline{"/root", "0", 48000}, 1}, 2};
+    path::fragment const frag_path_1b{path::channel{path::timeline{"/root", "0", 48000}, 1}, 2};
+    path::fragment const frag_path_2{path::channel{path::timeline{"/root", "0", 48000}, 1}, 3};
+
+    XCTAssertTrue((path::number_events{frag_path_1a}) == (path::number_events{frag_path_1b}));
+    XCTAssertFalse((path::number_events{frag_path_1a}) == (path::number_events{frag_path_2}));
+
+    XCTAssertFalse((path::number_events{frag_path_1a}) != (path::number_events{frag_path_1b}));
+    XCTAssertTrue((path::number_events{frag_path_1a}) != (path::number_events{frag_path_2}));
 }
 
 - (void)test_timeline_name {
