c29818ba529b1c9cd12b2c2ddbb53a94c286afca
Yuki Yasoshima <yukiyasos@gmail.com>
2021-01-17 13:28:38 +0900

    add method

--- playing_tests/yas_playing_player_task_tests.mm
+++ playing_tests/yas_playing_player_task_tests.mm
@@ -11,4 +11,24 @@
 
 @implementation yas_playing_player_task_tests
 
+- (void)test_reading_creating {
+#warning todo
+}
+
+- (void)test_buffering_creating {
+#warning todo
+}
+
+- (void)test_rendering_waiting {
+#warning todo
+}
+
+- (void)test_rendering_all_writing {
+#warning todo
+}
+
+- (void)test_rendering_advancing {
+#warning todo
+}
+
 @end
