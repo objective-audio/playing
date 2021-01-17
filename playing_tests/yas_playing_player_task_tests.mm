c60d53b78d03529d28c5d6129929743a728d9e17
Yuki Yasoshima <yukiyasos@gmail.com>
2021-01-17 13:24:12 +0900

    add player task tests

---
+++ playing_tests/yas_playing_player_task_tests.mm
@@ -0,0 +1,14 @@
+//
+//  yas_playing_player_task_tests.mm
+//
+
+#import <XCTest/XCTest.h>
+#import "yas_playing_player_test_utils.h"
+
+@interface yas_playing_player_task_tests : XCTestCase
+
+@end
+
+@implementation yas_playing_player_task_tests
+
+@end
