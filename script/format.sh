d974644a3db29bdbc039fa56a6047a214883defd
Yuki Yasoshima <yukiyasos@gmail.com>
2021-03-16 00:15:24 +0900

    update format.sh

--- script/format.sh
+++ script/format.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 
+export PATH=$PATH:/opt/homebrew/bin
 clang-format -i -style=file `find ../playing ../playing_tests ../playing_ios_sample/playing_ios_sample ../playing_mac_sample/playing_mac_sample ../playing_sample -type f \( -name *.h -o -name *.cpp -o -name *.hpp -o -name *.m -o -name *.mm \)`
